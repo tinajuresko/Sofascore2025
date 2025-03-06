83a7e54cec11c659acc8e7f5482371be48885559
tinajuresko <tina.juresko16@gmail.com>
2025-03-06 10:45:06 +0100

    commit

---
+++ homework1/homework1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  homework1
+//
+//  Created by Tina Jureško on 06.03.2025..
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
